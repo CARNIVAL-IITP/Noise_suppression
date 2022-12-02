a6249997ac775834795e66bb262aa643cb10578e
roar27 <roar27@hanyang.ac.kr>
2022-12-02 20:04:17 +0900

    training and inference code

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 from attrdict import AttrDict
 from losses.loss_util import get_lossfns
 from utils import AverageMeter
-import argparse, data, json, nn, numpy as np, os, time, torch
+import argparse, data, json, models, numpy as np, os, time, torch
 import glob, librosa
 # from data.feature_utils import get_istft
 import matplotlib.pyplot as plt
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 from itertools import permutations
 from pypesq import pesq
+import csv
+
 
 
 def get_free_gpu():
 
-    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    os.system('nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp')
     memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
     return np.argmax(memory_available)
 
@@ -31,45 +33,47 @@ class trainer:
             self.device = torch.device('cuda:%d'%available_device)
         else:
             self.device = torch.device('cpu')
-        # self.device = torch.device('cpu')
+        self.device = torch.device('cuda:2')
 
         # build model
-        self.model = self.init_model(args.model_name, args.model_options)
+        self.model = self.init_model(args.feature_options)
         print("Loaded the model...")
         # build loss fn
         self.loss_fn = self.build_lossfn(args.loss_option)
         print("Built the loss function...")
-        # build optimizer
-        self.optimizer = self.build_optimizer(self.model.parameters(), args.optimizer_options)
-        print("Built the optimizer...")
+        # # build optimizer
+        # self.optimizer = self.build_optimizer(self.model.parameters(), args.optimizer_options)
+        # print("Built the optimizer...")
         self.file_list = []
-        full_path = args.feature_options.data_path + 'tt' + '/mixed/*.wav'
+        full_path = args.feature_options.data_path + 'tt/mix/*.wav'
         self.file_list = glob.glob(full_path)
         self.feature_options = args.feature_options
         # build DataLoaders
-        if args.dataset == "wsj0-2mix":
-            self.test_loader = data.wsj0_2mix_inference_dataloader(args.model_name, args.feature_options, 'tt', args.cuda_option, self.device)
-        elif args.dataset == "daps":
-            self.train_loader = data.daps_enhance_dataloader(args.train_num_batch, args.feature_options, 'train', args.cuda_option, self.device)
-            self.valid_loader = data.daps_enhance_dataloader(args.vaildate_num_batch, args.feature_options, 'validation', args.cuda_option, self.device)
-        elif args.dataset == "edinburgh_tts":
-            self.train_loader = data.edinburgh_tts_dataloader(args.model_name, args.feature_options, 'train', args.cuda_option, self.device)
-            self.valid_loader = data.edinburgh_tts_dataloader(args.model_name, args.feature_options, 'validation', args.cuda_option, self.device)
-
-
+        self.test_loader = data.sitec_inference_dataloader(args.model_name, args.feature_options, 'tt', args.cuda_option, self.device)
+        
         # training options
         self.num_epoch = args.num_epoch
 
-        self.output_path = args.output_path+'{}50/'.format(self.model_name)
-        if not os.path.exists(self.output_path):
-            os.makedirs(self.output_path)
+        self.output_path = args.output_path+'{}/'.format(self.model_name)
+        if not os.path.exists(f'{self.output_path}/wav/'):
+            os.makedirs(f'{self.output_path}/wav/')
 
         self.min_loss = float("inf")
         self.early_stop_count = 0
 
-    def init_model(self, model_name, model_options):
-        model = torch.load('/home/dail/Workspace/DCCRN/output/DCCRN_mimo_DNS2020_4_snr_WPE_50/model.epoch70')
-        model.to(self.device)
+    def init_model(self,feature_options):
+        model = models.DCCRN_mimo(win_len=feature_options.window_size,
+                    win_inc=feature_options.hop_size, 
+                    use_clstm=True)
+        checkpoint = torch.load('./output/mimodccrn_MS_300ms_init_sitec_Sitec_snr_WPE/model.epoch147', map_location='cpu')
+        # model.load_state_dict(checkpoint['model'])
+        checkpoint = {key.replace("module.", ""): value for key, value in checkpoint['model'].items()}
+        
+        model.load_state_dict(checkpoint, strict=True)    
+        model.to(self.device)        
+        model.eval()
+        # model.load_state_dict(checkpoint['model'])
+        # model.to(self.device)
         return model
 
 
@@ -90,7 +94,7 @@ class trainer:
         # self.train(epoch)
         self.inference()
 
-        print("Model training is finished.")
+        print("Model inferencing is finished.")
 
     def inference(self):
         # losses = AverageMeter()
@@ -101,30 +105,56 @@ class trainer:
         end = time.time()
         pesqs = []
         snris = []
+        pesqs_n = []
+        with open(self.output_path + '/out.csv', 'w', newline='') as f:
+            wr = csv.writer(f)
+            wr.writerow(['filename', 'sisnri', 'noisy_pesq', 'pesq'])
         with torch.no_grad():
             for i, data in enumerate(self.test_loader):
-                begin = time.time()
-                input, label, infdat = data
-                input = [ele.to(self.device) for ele in input]
-                label = [ele.to(self.device) for ele in label]
+                input, input_frames, label, noise_MS, infdat = data
+                input_frames = input_frames.to(self.device).float()
+                label = label.to(self.device).float()
+                input = input.to(self.device).float()
+                noise_MS = noise_MS.to(self.device).float()
+                    
 
-                est_phase, out_wav, specs_t = self.model(input, label)
-                loss, loss_snr, WPE, snri = self.loss_fn(input, out_wav, label, est_phase, specs_t)
+                batch, channel, length = input.shape
+                out_wav, specs_o, specs_t = self.model(input_frames, label, noise_MS)
+                
+                loss, loss_snr, WPE, snri = self.loss_fn(input,  out_wav, label, specs_o, specs_t)
+                
+                # out_wavs = out_wav.squeeze(dim=0)
+                # frames_cat = [out_wav[x, :, :1664] for x in range(95)]
+                # frames_cat.append(out_wavs[-1, :, 1664:])
+                # wav_out = torch.cat(frames_cat, -1)
+                
+                # frames_catl = [label[0, x, :, :1664] for x in range(95)]
+                # frames_catl.append(label[0, -1, :, 1664:])
+                # label_out = torch.cat(frames_catl, -1)
+    
+                
+                label_out = label[0].cpu().detach().numpy()
+                wav_out = out_wav[0].cpu().detach().numpy()
+                input = input[0].cpu().detach().numpy()
+                # if label.shape[-1] > audio_out.shape[-1]:
+                #     label = label[...,:audio_out.shape[-1]]
+                length = label_out.shape[0]
 
-                out_wavs = out_wav.squeeze(dim=1)
-                audio_out = out_wavs.cpu().detach().numpy()
-                label = label[0][0].cpu().detach().numpy()
-                if label.shape[-1] > audio_out.shape[-1]:
-                    label = label[:,:audio_out.shape[-1]]
-                length = label.shape[0]
+                result = sum([pesq(s, t) for s, t in zip(label_out, wav_out)])/length
+                result_n = sum([pesq(s, t) for s, t in zip(label_out, input)])/length
 
-                result = sum([pesq(s, t) for s, t in zip(label, audio_out[0])])/length
                 pesqs.append(result)
+                pesqs_n.append(result_n)
                 snris.append(snri)
 
-                fn = self.output_path + os.path.split(self.file_list[i])[-1]
-                audio_out = audio_out.squeeze().T
-                # sf.write(fn.replace('.wav', '_{}.wav'.format(result)), audio_out, self.feature_options.sampling_rate, subtype='PCM_16')
+                with open(f'{self.output_path}/out.csv', 'a', newline='') as f:
+                    wr = csv.writer(f)
+                    wr.writerow([os.path.split(self.file_list[i])[-1], snri.cpu().detach().numpy(),result_n, result])
+
+                fn = f'{self.output_path}/wav/' + os.path.split(self.file_list[i])[-1]
+
+                wav_out = wav_out.squeeze().T
+                sf.write(fn.replace('.wav', '_{}.wav'.format(round(result,2))), wav_out, self.feature_options.sampling_rate, subtype='PCM_16')
                 times.update(time.time() - end)
                 end = time.time()
                 print('%d/%d, pesq: {}, time estimated: %.2f seconds' .format(result) % (i + 1, len_d, times.avg * len_d), end='\r')
--- train.py
+++ train.py
@@ -1,13 +1,32 @@
 from attrdict import AttrDict
 from losses.loss_util import get_lossfns
-from utils import AverageMeter
-import argparse, data, json, nn, numpy as np, os, time, torch
+import argparse, data, json, models, numpy as np, os, time, torch
 from torch.utils.tensorboard import SummaryWriter
 import wandb
+import torch.nn as nn
+import torch.optim as optim
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 def get_free_gpu():
-    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    os.system('nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp')
     memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
     return np.argmax(memory_available)
 
@@ -25,32 +44,31 @@ class trainer:
         else:
             self.device = torch.device('cpu')
         # build model
-        self.model = self.init_model(args.model_name, args.model_options)
-        print("Loaded the model...")
+        self.model = models.DCCRN_mimo(win_len=args.feature_options.window_size,
+                    win_inc=args.feature_options.hop_size, 
+                    use_clstm=True)
+        checkpoint = torch.load('./best', map_location='cpu')
+        self.model.load_state_dict(checkpoint['model'])
+        self.model.to(self.device)
+       
         # build loss fn
         self.loss_fn = self.build_lossfn(args.loss_option)
-        print("Built the loss function...")
         # build optimizer
         self.optimizer = self.build_optimizer(self.model.parameters(), args.optimizer_options)
-        print("Built the optimizer...")
+        self.scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer=self.optimizer, T_max=50, eta_min=0)
+    
         # build DataLoaders
-        if args.dataset == "DNS2020_4":
-            self.train_loader = data.wsj0_2mix_dataloader(args.model_name, args.feature_options, 'tr', args.cuda_option, self.device)
-            self.valid_loader = data.wsj0_2mix_dataloader(args.model_name, args.feature_options, 'cv', args.cuda_option, self.device)
+        self.train_loader = data.sitec_dataloader(args.model_name, args.feature_options, 'tr', args.cuda_option, self.device)
+        self.valid_loader = data.sitec_dataloader(args.model_name, args.feature_options, 'cv', args.cuda_option, self.device)
 
         # training options
         self.num_epoch = args.num_epoch
-        self.output_path = args.output_path+'%s_%s_%s_50'%(self.model_name, self.dataset, self.loss_name)
+        self.output_path = args.output_path+'%s_%s_%s'%(self.model_name, self.dataset, self.loss_name)
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
         self.min_loss = float("inf")
         self.early_stop_count = 0
-
-    def init_model(self, model_name, model_options):
-        model = nn.DCCRN_mimo(use_clstm=True)
-        wandb.watch(model)
-        model.to(self.device)
-        return model
+        self.final_loss = 0
 
 
     def build_lossfn(self, fn_name):
@@ -64,40 +82,37 @@ class trainer:
             return torch.optim.SGD(params, lr=optimizer_options.lr, momentum=0.9)
         if optimizer_options.name == "rmsprop":
             return torch.optim.RMSprop(params, lr=optimizer_options.lr)
+        if optimizer_options.name == "adamW":
+            return torch.optim.AdamW(params, lr=optimizer_options.lr)
 
     def run(self):
         for epoch in range(self.num_epoch):
+            print(self.model_name)
             self.train(epoch)
             self.validate(epoch)
-            if self.early_stop_count == 10:
-                print("Model stops improving, stop the training")
-                break
+            self.scheduler.step()
         print("Model training is finished.")
 
 
     def train(self, epoch):
         losses = AverageMeter()
         times = AverageMeter()
-
         losses_snr = AverageMeter()
-
         losses.reset()
         times.reset()
-
         losses_snr.reset()
-
         self.model.train()
-
         len_d = len(self.train_loader)
         end = time.time()
         for i, data in enumerate(self.train_loader):
-            input, label= data
-
-            input = [ele.to(self.device) for ele in input]
-            label = [ele.to(self.device) for ele in label]
-
-            est_phase, out_wav, specs_t = self.model(input, label)
-            loss, loss_snr, WPE = self.loss_fn(out_wav, label, est_phase, specs_t)
+            input, input_frames, label, noise_MS = data
+            input_frames = input_frames.to(self.device).float()
+            label = label.to(self.device).float()
+            input = input.to(self.device).float()
+            noise_MS = noise_MS.to(self.device).float()
+            out_wav, specs_o, specs_t = self.model(input_frames, label, noise_MS)
+            
+            loss, loss_snr, WPE, snri = self.loss_fn(input,  out_wav, label, specs_o, specs_t)
             losses.update(loss.item())
             self.optimizer.zero_grad()
             loss.backward()
@@ -112,7 +127,7 @@ class trainer:
             wandb.log({
                 "train_loss/loss": losses.avg,
                 "train_WPE": WPE,
-                "train_snr": loss_snr
+                "train_snr": loss_snr,
             })
         print("\n")
 
@@ -121,31 +136,25 @@ class trainer:
         self.model.eval()
         losses = AverageMeter()
         times = AverageMeter()
-
         losses_snr = AverageMeter()
-
         losses.reset()
         times.reset()
-
         losses_snr.reset()
-
         len_d = len(self.valid_loader)
         end = time.time()
         with torch.no_grad():
             for i, data in enumerate(self.valid_loader):
-                begin = time.time()
-                input, label = data
-
-                input = [ele.to(self.device) for ele in input]
-                label = [ele.to(self.device) for ele in label]
-
-                est_phase,  out_wav, specs_t = self.model(input, label)
-                loss, loss_snr, WPE = self.loss_fn(out_wav, label, est_phase, specs_t)
-
+                input, input_frames, label, noise_MS = data
+                input_frames = input_frames.to(self.device).float()
+                label = label.to(self.device).float()
+                input = input.to(self.device).float()
+                noise_MS = noise_MS.to(self.device).float()
+                out_wav, specs_o, specs_t = self.model(input_frames, label, noise_MS)
+                
+                loss, loss_snr, WPE, snri = self.loss_fn(input,  out_wav, label, specs_o, specs_t)           
                 losses.update(loss.item())
                 losses_snr_avg = torch.mean(loss)
                 losses_snr.update(losses_snr_avg.item())
-
                 times.update(time.time() - end)
                 end = time.time()
                 writer.add_scalar('valid_loss/loss(snr)', losses.avg, epoch * len_d + i + 1)
@@ -153,17 +162,25 @@ class trainer:
                 wandb.log({
                     "valid_loss": losses.avg,
                     "valid_WPE": WPE,
-                    "valid_snr": loss_snr
+                    "valid_snr": loss_snr,
                 })
             print("\n")
-
-        if losses.avg < self.min_loss:
+        self.final_loss = losses.avg
+        if self.final_loss < self.min_loss:
             self.early_stop_count = 0
-            self.min_loss = losses.avg
-            torch.save(self.model,self.output_path+"/model.epoch%d"%epoch)
-            print("Saved new model")
+            checkpoint = {
+                'epoch': epoch + 1,
+                'model': self.model.state_dict(),
+                'optimizer': self.optimizer.state_dict()
+            }
+            if epoch>-1:
+                torch.save(checkpoint, self.output_path+"/model.epoch%d"%epoch)
+                torch.save(checkpoint, self.output_path+"/best")
+                print('model is saved.')
+            self.min_loss = self.final_loss
         else:
             self.early_stop_count += 1
+
 
 
     def evaluate(self):
@@ -172,21 +189,19 @@ class trainer:
 
 def main():
     wandb.init()
-    wandb.run.name = 'DCCRN_mimo'
     parser = argparse.ArgumentParser(description='Parse the config path')
-    parser.add_argument("-c", "--config", dest="path", default='./configs/speakerbeam_timitdb_config.json',
+    parser.add_argument("-c", "--config", dest="path", default='./configs/train.json',
                         help='The path to the config file. e.g. python train.py --config configs/dc_config.json')
 
     config = parser.parse_args()
     with open(config.path) as f:
         args = json.load(f)
         args = AttrDict(args)
-    wandb.config.update(args)
     t = trainer(args)
     t.run()
 
 
 if __name__ == "__main__":
-    writer = SummaryWriter("./tensorboard/speakerbeam")
+    writer = SummaryWriter("./tensorboard")
     main()
     writer.close()
