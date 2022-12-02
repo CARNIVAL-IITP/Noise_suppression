ee3567b09f58d3b5802aee2e1e51fe37d23c7acb
roar27 <roar27@hanyang.ac.kr>
2022-12-02 20:05:44 +0900

    loss code

--- losses/loss_util.py
+++ losses/loss_util.py
@@ -5,24 +5,11 @@ The loss function should take two arguments:
     label: a tuple which is from dataloader
 You need to assert the format of the output and label in the loss function!
 """
-from .loss_chimera import loss_chimera_msa, loss_chimera_psa
-from .loss_speakerbeam import loss_speakerbeam_psa
-from .loss_dc import loss_dc
-from .loss_enhancement import loss_mask_msa, loss_mask_psa
-from .loss_phase import loss_phase
-from .calc_sdr import calc_sdr
+from .loss import loss_wpe
+
 from attrdict import AttrDict
 
 def get_lossfns():
     loss_fns = AttrDict()
-    loss_fns["loss_dc"] = loss_dc
-    loss_fns["loss_chimera_msa"] = loss_chimera_msa
-    loss_fns["loss_chimera_psa"] = loss_chimera_psa
-    loss_fns["loss_phase"] = loss_phase
-    loss_fns["loss_mask_msa"] = loss_mask_msa
-    loss_fns["loss_mask_psa"] = loss_mask_psa
-    # loss_fns["loss_speakerbeam"] = calc_sdr
-    loss_fns["loss_speakerbeam"] = loss_speakerbeam_psa
-    loss_fns["snr_WPE"] = loss_speakerbeam_psa
-    loss_fns["calc_sdr"] = calc_sdr
+    loss_fns["wpe"] = loss_wpe
     return loss_fns
