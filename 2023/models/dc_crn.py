c6f55e2e1a5dd811add0022461d715a2082f48bc
dail <roar27@hanyang.ac.kr>
2023-12-21 16:44:18 +0900

    models

---
+++ 2023/models/dc_crn.py
@@ -0,0 +1,341 @@
+import torch
+import torch.nn as nn
+import os
+import sys
+# from nn.show import show_params, show_model
+import torch.nn.functional as F
+from models.conv_stft import ConvSTFT, ConviSTFT
+import numpy as np, random
+from models.complexnn import ComplexConv2d, ComplexConvTranspose2d, NavieComplexLSTM, complex_cat, ComplexBatchNorm
+
+class DCCRN_mimo(nn.Module):
+
+    def __init__(
+                    self, 
+                    rnn_layers=2,
+                    rnn_units=128,
+                    win_len=400,
+                    win_inc=100, 
+                    fft_len=512,
+                    win_type='hanning',
+                    masking_mode='E',
+                    use_clstm=True,
+                    use_cbn = False,
+                    kernel_size=5,
+                    kernel_num=[32,64,128,128,256,256],
+                    channel=8,
+                    teacher_model=None
+                ):
+        ''' 
+            
+            rnn_layers: the number of lstm layers in the crn,
+            rnn_units: for clstm, rnn_units = real+imag
+
+        '''
+
+        super(DCCRN_mimo, self).__init__()
+
+        # for fft 
+        self.win_len = win_len
+        self.win_inc = win_inc
+        self.fft_len = fft_len
+        self.win_type = win_type 
+
+        self.teacher_model = teacher_model
+        input_dim = win_len
+        output_dim = win_len
+        
+        self.rnn_units = rnn_units
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_layers = rnn_layers
+        self.kernel_size = kernel_size
+        self.channel = channel
+        self.kernel_num = [2*self.channel]+kernel_num
+        self.masking_mode = masking_mode
+        self.use_clstm = use_clstm
+
+        bidirectional=False
+        fac = 2 if bidirectional else 1
+        fix = True
+        self.fix = fix
+
+        self.stft = ConvSTFT(self.win_len, self.win_inc, fft_len, self.win_type, 'complex', fix=fix)
+        self.istft = ConviSTFT(self.win_len, self.win_inc, fft_len, self.win_type, 'complex', fix=fix)
+        self.encoder = nn.ModuleList()
+        for idx in range(len(self.kernel_num)-1):
+            self.encoder.append(
+                nn.Sequential(
+                    ComplexConv2d(
+                        self.kernel_num[idx],
+                        self.kernel_num[idx+1],
+                        kernel_size=(self.kernel_size, 2),
+                        stride=(2, 1),
+                        padding=(2, 1)
+                    ),
+                    nn.BatchNorm2d(self.kernel_num[idx+1]) if not use_cbn else ComplexBatchNorm(self.kernel_num[idx+1]),
+                    nn.PReLU()
+                )
+            )
+        hidden_dim = self.fft_len//(2**(len(self.kernel_num))) 
+
+        if self.use_clstm: 
+            rnns = []
+            for idx in range(rnn_layers):
+                rnns.append(
+                        NavieComplexLSTM(
+                        input_size= hidden_dim*self.kernel_num[-1] if idx == 0 else self.rnn_units,
+                        hidden_size=self.rnn_units,
+                        bidirectional=bidirectional,
+                        batch_first=False,
+                        projection_dim= hidden_dim*self.kernel_num[-1] if idx == rnn_layers-1 else None,
+                        )
+                    )
+                self.enhance = nn.Sequential(*rnns)
+        else:
+            self.enhance = nn.LSTM(
+                    input_size= hidden_dim*self.kernel_num[-1],
+                    hidden_size=self.rnn_units,
+                    num_layers=2,
+                    dropout=0.0,
+                    bidirectional=bidirectional,
+                    batch_first=False
+            )
+            self.tranform = nn.Linear(self.rnn_units * fac, hidden_dim*self.kernel_num[-1])
+
+        self.decoder = nn.ModuleList()
+        for idx in range(len(self.kernel_num)-1, 0, -1):
+            if idx != 1:
+                self.decoder.append(
+                    nn.Sequential(
+                        ComplexConvTranspose2d(
+                        self.kernel_num[idx]*2,
+                        self.kernel_num[idx-1],
+                        kernel_size =(self.kernel_size, 2),
+                        stride=(2, 1),
+                        padding=(2, 0),
+                        output_padding=(1, 0)
+                    ),
+                    nn.BatchNorm2d(self.kernel_num[idx-1]) if not use_cbn else ComplexBatchNorm(self.kernel_num[idx-1]),
+                    #nn.ELU()
+                    nn.PReLU()
+                    )
+                )
+            else:
+                self.decoder.append(
+                    nn.Sequential(
+                        ComplexConvTranspose2d(
+                        self.kernel_num[idx]*2,
+                        self.kernel_num[idx-1],
+                        kernel_size =(self.kernel_size, 2),
+                        stride=(2, 1),
+                        padding=(2,0),
+                        output_padding=(1,0)
+                    ),
+                    )
+                )
+
+        self.flatten_parameters() 
+
+        self.scale = nn.Conv2d(8, 256, (130, 1), stride=(32,1))
+        self.shift = nn.Conv2d(8, 256, (130, 1), stride=(32,1))
+
+        # self.scale_t = nn.Conv2d(1, 256, (130, 1), stride=(32,1))
+        # self.shift_t = nn.Conv2d(1, 256, (130, 1), stride=(32,1))
+        
+
+    def flatten_parameters(self): 
+        if isinstance(self.enhance, nn.LSTM):
+            self.enhance.flatten_parameters()
+
+    def forward(self, inp, label, noise, noise_est,  state=None, teacher=False):
+
+        batch = inp.shape[0]
+        inputs = inp.reshape(-1, inp[0].shape[-1])   # (B*C, T)
+        specs = self.stft(inputs)  # (B, C, F, T)
+        specs = specs.reshape(batch, self.channel, self.fft_len+2, -1)
+       
+        real = specs[:,:, :self.fft_len//2+1]
+        imag = specs[:,:, self.fft_len//2+1:]
+
+        spec_mags = torch.sqrt(real**2+imag**2+1e-8)
+        spec_mags = spec_mags
+        spec_phase = torch.atan2(imag, real)
+        spec_phase = spec_phase
+
+        cspecs = torch.cat([real, imag], dim=1)
+        # cspecs = torch.stack([real,imag],1)
+        cspecs = cspecs[:,:, 1:,:]
+        
+        out = cspecs
+        encoder_out = []
+        
+        for idx, layer in enumerate(self.encoder):
+            out = layer(out)
+            
+            encoder_out.append(out)
+
+        if teacher: # teacher version
+            # print('teacher')
+            shift = self.shift(noise)  #(batch, 256, 4, T)
+            scale = self.scale(noise)
+        else:
+            # print('student')
+            shift = self.shift(noise_est)  #(batch, 256, 4, T)
+            scale = self.scale(noise_est)
+        
+        out = scale*out+shift
+        batch_size, channels, dims, lengths = out.size()
+        out = out.permute(3, 0, 1, 2)
+        
+
+        if self.use_clstm:
+            r_rnn_in = out[:,:,:channels//2]
+            i_rnn_in = out[:,:,channels//2:]
+            r_rnn_in = torch.reshape(r_rnn_in, [lengths, batch_size, channels//2*dims])
+            i_rnn_in = torch.reshape(i_rnn_in, [lengths, batch_size, channels//2*dims])
+        
+            r_rnn_in, i_rnn_in = self.enhance([r_rnn_in, i_rnn_in])
+
+            r_rnn_in = torch.reshape(r_rnn_in, [lengths, batch_size, channels//2, dims])
+            i_rnn_in = torch.reshape(i_rnn_in, [lengths, batch_size, channels//2, dims]) 
+            out = torch.cat([r_rnn_in, i_rnn_in],2)
+        
+        else:
+            # to [L, B, C, D]
+            out = torch.reshape(out, [lengths, batch_size, channels*dims])
+            out, _ = self.enhance(out)
+            out = self.tranform(out)
+            out = torch.reshape(out, [lengths, batch_size, channels, dims])
+       
+        out = out.permute(1, 2, 3, 0)
+        
+        for idx in range(len(self.decoder)):
+            out = complex_cat([out,encoder_out[-1 - idx]],1)
+            out = self.decoder[idx](out)
+            out = out[...,1:]
+
+        mask_real = out[:, :self.channel, :,:]
+        mask_imag = out[:, self.channel:, :, :]
+        mask_real = F.pad(mask_real, [0,0,1,0])
+        mask_imag = F.pad(mask_imag, [0,0,1,0])
+        
+        if self.masking_mode == 'E' :
+            mask_mags = (mask_real**2+mask_imag**2)**0.5
+            real_phase = mask_real/(mask_mags+1e-8)
+            imag_phase = mask_imag/(mask_mags+1e-8)
+            mask_phase = torch.atan2(
+                            imag_phase,
+                            real_phase
+                        ) 
+
+            # mask_mags = torch.clamp_(mask_mags,0,100)
+            mask_mags = torch.tanh(mask_mags)
+            est_mags = mask_mags*spec_mags 
+            est_phase = spec_phase + mask_phase
+            real = est_mags*torch.cos(est_phase)
+            imag = est_mags*torch.sin(est_phase) 
+        elif self.masking_mode == 'C':
+            real,imag = real*mask_real-imag*mask_imag, real*mask_imag+imag*mask_real
+        elif self.masking_mode == 'R':
+            real, imag = real*mask_real, imag*mask_imag 
+        
+        out_spec = torch.cat([real, imag], 2)
+
+        out_wav = self.istft(out_spec.reshape(batch*self.channel, self.fft_len+2, -1))
+
+        targets = label.reshape(-1, label[0].shape[-1])
+        specs_t = self.stft(targets).reshape(batch, self.channel, self.fft_len + 2, -1)
+
+        if self.teacher_model != None:
+            out_wav_t, est_phase_t, _ = self.teacher_model(inp, label, noise, noise_est, teacher=True)
+            return out_wav_t, est_phase_t, out_wav.reshape(batch, self.channel, -1), est_phase, specs_t
+
+        else:
+            return out_wav.reshape(batch, self.channel, -1), est_phase, specs_t
+
+    def get_params(self, weight_decay=0.0):
+            # add L2 penalty
+        weights, biases = [], []
+        for name, param in self.named_parameters():
+            if 'bias' in name:
+                biases += [param]
+            else:
+                weights += [param]
+        params = [{
+                     'params': weights,
+                     'weight_decay': weight_decay,
+                 }, {
+                     'params': biases,
+                     'weight_decay': 0.0,
+                 }]
+        return params
+
+    def loss(self, inputs, labels, loss_mode='SI-SNR'):
+       
+        if loss_mode == 'MSE':
+            b, d, t = inputs.shape 
+            labels[:, 0, :]=0
+            labels[:, d//2, :]=0
+            return F.mse_loss(inputs, labels, reduction='mean')*d
+
+        elif loss_mode == 'SI-SNR':
+            # return -torch.mean(si_snr(inputs, labels))
+            return -(si_snr(inputs, labels))
+        elif loss_mode == 'MAE':
+            gth_spec, gth_phase = self.stft(labels) 
+            b,d,t = inputs.shape 
+            return torch.mean(torch.abs(inputs-gth_spec))*d
+
+def remove_dc(data):
+    mean = torch.mean(data, -1, keepdim=True) 
+    data = data - mean
+    return data
+def l2_norm(s1, s2):
+    #norm = torch.sqrt(torch.sum(s1*s2, 1, keepdim=True))
+    #norm = torch.norm(s1*s2, 1, keepdim=True)
+    
+    norm = torch.sum(s1*s2, -1, keepdim=True)
+    return norm 
+
+def si_snr(s1, s2, eps=1e-8):
+    #s1 = remove_dc(s1)
+    #s2 = remove_dc(s2)
+    s1_s2_norm = l2_norm(s1, s2)
+    s2_s2_norm = l2_norm(s2, s2)
+    s_target =  s1_s2_norm/(s2_s2_norm+eps)*s2
+    e_nosie = s1 - s_target
+    target_norm = l2_norm(s_target, s_target)
+    noise_norm = l2_norm(e_nosie, e_nosie)
+    snr = 10*torch.log10((target_norm)/(noise_norm+eps)+eps)
+    return torch.mean(snr)
+
+def test_complex():
+    torch.manual_seed(20)
+    inputs = torch.randn(10,2,256,10)
+    conv = ComplexConv2d(2,32,(3,1),(2,1),(1,0))
+    tconv = ComplexConvTranspose2d(32,2,(3,1),(2,1),(1,0),(1,0))
+    out = conv(inputs)
+    print(out.shape)
+    out = tconv(out)
+    print(out.shape)
+
+if __name__ == '__main__':
+    from thop import profile, clever_format
+    # DCCRN-CL
+    device = 'cuda'
+    with torch.no_grad():
+        model = DCCRN_mimo()
+        model.to(device)
+        # total_params = sum(p.numel() for p in model.parameters())
+        # print("total parmas: ", total_params*1e-6)
+        wav = torch.randn(1, 1, 4, 160000)
+        # print(wav[0,0,0,:10])
+        # exit()
+        out = model([wav.to(device)], [wav.to(device)])
+        exit()
+
+        # print(device, model, wav)
+        macs, params = profile(model, inputs=([wav.to(device)], [wav.to(device)]))
+        macs, params = clever_format([macs, params], "%.3f")
+        print(macs, params)
