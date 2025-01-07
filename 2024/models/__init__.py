8a181c78d4cde994a44a03d2b6711cd37ede0f12
dail <roar27@hanyang.ac.kr>
2025-01-07 17:18:08 +0900

    __init__.py

---
+++ 2024/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+For every model created under nn directory. Please import it here.
+The model should takes a list of tensors named "input" as the input,
+and also returns a list of tensors as the output.
+"""
+
+
+from .dc_crn import DCCRN_mimo
